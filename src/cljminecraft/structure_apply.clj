402249ad542c70552d8cef6411aac52a9aaebb55
kolen <incredible.angst@gmail.com>
2015-09-21 02:49:54 +0300

    apply-to-world fixes

--- src/cljminecraft/structure_apply.clj
+++ src/cljminecraft/structure_apply.clj
@@ -2,14 +2,21 @@
   (:require [cljminecraft.structure :as s])
   (:import [org.bukkit Location]))
 
+(defn- apply-block
+  [material-data location]
+  (let [state (-> location .getBlock .getState)
+        old-material-data (.getData state)]
+    (.setType state (.getItemType material-data))
+    (.setData state material-data)
+    (.update state true false) ;force=true, applyPhysics=false
+    old-material-data))
+
 (defn apply-to-world
   [struct location]
   (s/map-with-pos struct
                   (fn [x y z new-material-data]
-                    (let [newloc (-> (.clone location) (.add x y z))
-                          state (-> newloc .getBlock .getState)
-                          old-material-data (.getData state)]
-                      (.setType state (.getItemType new-material-data))
-                      (.setData state new-material-data)
-                      (.update state true false) ;force=true, applyPhysics=false
-                      old-material-data))))
+                    (if (nil? new-material-data)
+                      nil
+                      (let [newloc (-> (.clone location) (.add x y z))]
+                        (prn (.getX newloc) (.getY newloc) (.getZ newloc))
+                        (apply-block new-material-data newloc))))))
