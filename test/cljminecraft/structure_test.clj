8c68ba10c390459cec92ff13a91ab92ee144b8b0
kolen <incredible.angst@gmail.com>
2015-09-23 01:26:56 +0300

    One more test for merge (failing)

--- test/cljminecraft/structure_test.clj
+++ test/cljminecraft/structure_test.clj
@@ -54,7 +54,12 @@
                       "11222"]
                      ["111  "
                       "111  "
-                      "111  "]))))
+                      "111  "])))
+  (is (= (s/merge (-> (s/box 1 2 1 :fill 1) (s/move 1 0 0))
+                (s/box 2 1 1 :fill 2))
+         (s/from-str {\1 1 \2 2}
+                     [" 1"
+                      "22"]))))
 
 (deftest box-small
   (is (= (s/box 3 1 1 :fill 1)
